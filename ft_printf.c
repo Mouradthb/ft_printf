e3c5cb763b381945dabb984691a2de89368f46e3
Mouradthb <mouradthb@outlook.fr>
2022-11-16 14:48:41 +0100

    printf_putstr protrcted

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,28 @@
 /*   By: mthabit <mthabit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 17:55:00 by mthabit           #+#    #+#             */
-/*   Updated: 2022/11/15 20:46:55 by mthabit          ###   ########.fr       */
+/*   Updated: 2022/11/16 11:59:47 by mthabit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	check_form(const char c, va_list va, int count)
+static int	check_form(const char c, va_list argptr, int count)
 {
 	if (c == 'd' || c == 'i')
-		ft_putnbr(va_arg(va, int), count);
+		ft_putnbr(va_arg(argptr, int), count);
 	else if (c == 'x' || c == 'X')
-		ft_puthexa(va_arg(va, unsigned int), c, count);
+		ft_puthexa(va_arg(argptr, unsigned int), c, count);
 	else if (c == 'p')
-		ft_putadress(va_arg(va, void *), count);
+		ft_putadress(va_arg(argptr, void *), count);
 	else if (c == 'u')
-		ft_putnbru(va_arg(va, unsigned int), count);
+		ft_putnbru(va_arg(argptr, unsigned int), count);
 	else if (c == 's')
-		ft_putstr(va_arg(va, char), count);
+		ft_putstr(va_arg(argptr, char *), count);
 	else if (c == 'c')
-		ft_putchar(va_arg(va, char), count);
+		ft_putchar(va_arg(argptr, int), count);
+	else if (c == '%')
+		ft_putchar(c, count);
 	else
 		ft_putchar(c, count);
 	return (count);
@@ -33,26 +35,26 @@ static int	check_form(const char c, va_list va, int count)
 
 int	ft_printf(const char *s, ...)
 {
-	va_list	va;
+	va_list	argptr;
 	int		count;
 	int		i;
 
 	count = 0;
 	i = 0;
-	va_start(va, s);
+	va_start(argptr, s);
 	while (s[i])
 	{
 		if (s[i] == '%')
 		{
 			if (s[i + 1] == '\0')
 				break ;
-			check_form(s[i + 1], va, &count);
+			check_form(s[i + 1], argptr, &count);
 			i++;
 		}
 		else
 			ft_putchar(s[i], &count);
 		i++;
 	}
-	va_end(va);
+	va_end(argptr);
 	return (count);
 }
--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: mthabit <mthabit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 16:51:01 by mthabit           #+#    #+#             */
-/*   Updated: 2022/11/15 17:35:15 by mthabit          ###   ########.fr       */
+/*   Updated: 2022/11/16 14:40:46 by mthabit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 void	ft_putstr(char *s, int *count)
 {
+	int	i;
+
 	if (!s)
-		return ;
-	while (*s != '\0')
 	{
-		ft_putchar(*s, count);
-		s++;
+		ft_putstr("(null)", count);
+		return ;
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ft_putchar(s[i], count);
+		i++;
 	}
 }
